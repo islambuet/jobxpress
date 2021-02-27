2bdf2f5b8b38fc86b7be3444a25d0c8ce3e3cbea
Md. Shaiful Islam <islambuet@gmail.com>
2021-02-27 10:27:15 +0600

    Admin middleware included

---
+++ app/Http/Middleware/adminMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Auth;
+
+class adminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+       if(Auth::user())
+       {
+           if(Auth::user()->is_admin)
+           {
+            return $next($request);
+           }
+           else
+           {
+            return redirect('/');
+
+           }
+       }
+       return redirect('admin/login')->with('error','You have not admin access');
+    }
+}
