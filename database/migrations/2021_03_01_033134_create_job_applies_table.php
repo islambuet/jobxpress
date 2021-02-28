adffc09f44aa32beda8c7e23d6f2bc69c337751d
Md. Shaiful Islam <islambuet@gmail.com>
2021-03-01 03:49:50 +0600

    apply job table created

---
+++ database/migrations/2021_03_01_033134_create_job_applies_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateJobAppliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_applies', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('job_id');
+            $table->string('token',80);
+            $table->string('applied',3)->default('No');
+            $table->string('name');
+            $table->string('email');
+            $table->string('resume',500);
+            $table->string('phone')->nullable();
+            $table->string('employer')->nullable();
+            $table->string('source')->nullable();
+            $table->text('comments')->nullable();
+            $table->timestamp('token_expired_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_applies');
+    }
+}
